b24b0335d3a28d85d9a3f0d2bcb3c2990808b8aa
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-07-24 10:42:21 +0300

    card component

---
+++ src/CryptoCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CryptoCard = () => {
+  return (
+    <div>
+      <h2>Crypto Card</h2>
+      <p>This is a crypto card component.</p>
+    </div>
+  );
+};
+
+export default CryptoCard;
